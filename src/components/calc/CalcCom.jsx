f3e574cf7b63060c29db832f6b579908683a4adf
nilesh7272 <nileshparadva97@gmail.com>
2024-10-16 16:29:16 +0530

    React Calc

--- src/components/calc/CalcCom.jsx
+++ src/components/calc/CalcCom.jsx
@@ -5,54 +5,40 @@ const CalcCom = () => {
     const [input, setInput] = useState("");
     const [result, setResult] = useState("");
 
-    const handleButtonClick = (value) => {
+    const buttonClick = (value) => {
         setInput((prev) => prev + value);
     };
 
-    const handleCalculate = () => {
+    const Calculate = () => {
         try {
-            const calculatedResult = eval(input);
-            setResult(calculatedResult);
+            const calResult = eval(input);
+            setResult(calResult);
         } catch (error) {
             setResult("Error");
         }
     };
 
-    const handleClear = () => {
+    const Clear = () => {
         setInput("");
         setResult("");
     };
 
     return (
-        <div className="calculator">
-            <div className="display">
-                <div className="input">{input}</div>
-                <div className="result">{result}</div>
+        <>
+            <div className="calculator">
+                <div className="display">
+                    <div className="input">{input}</div>
+                    <div className="result">{result}</div>
+                </div>
+                <div className="buttons">
+                    {["7", "8", "9", "/"].map((item) => (<button key={item} onClick={() => buttonClick(item)}>{item}</button>))}
+                    {["4", "5", "6", "*"].map((item) => (<button key={item} onClick={() => buttonClick(item)}>{item}</button>))}
+                    {["1", "2", "3", "-"].map((item) => (<button key={item} onClick={() => buttonClick(item)}>{item}</button>))}
+                    {["0", ".", "=", "+"].map((item) => (<button key={item} onClick={() => item === "=" ? Calculate() : buttonClick(item)}>{item}</button>))}
+                    <button className="clear" onClick={Clear}>C</button>
+                </div>
             </div>
-            <div className="buttons">
-                {["7", "8", "9", "/"].map((item) => (
-                    <button key={item} onClick={() => handleButtonClick(item)}>
-                        {item}
-                    </button>
-                ))}
-                {["4", "5", "6", "*"].map((item) => (
-                    <button key={item} onClick={() => handleButtonClick(item)}>
-                        {item}
-                    </button>
-                ))}
-                {["1", "2", "3", "-"].map((item) => (
-                    <button key={item} onClick={() => handleButtonClick(item)}>
-                        {item}
-                    </button>
-                ))}
-                {["0", ".", "=", "+"].map((item) => (
-                    <button key={item} onClick={() => item === "=" ? handleCalculate() : handleButtonClick(item)}>
-                        {item}
-                    </button>
-                ))}
-                <button className="clear" onClick={handleClear}>C</button>
-            </div>
-        </div>
+        </>
     );
 };
 
